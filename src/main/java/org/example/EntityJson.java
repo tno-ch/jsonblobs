25fb7c02036bad12de1975a1daf804c733bebeee
Chico Hau <chico.hau@tno.nl>
2024-04-03 16:02:59 +0200

    added PostgresqlTest

---
+++ src/main/java/org/example/EntityJson.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright TNO Geologische Dienst Nederland
+ *
+ * Alle rechten voorbehouden.
+ * Niets uit deze software mag worden vermenigvuldigd en/of openbaar gemaakt door middel van druk, fotokopie,
+ * microfilm of op welke andere wijze dan ook, zonder voorafgaande toestemming van TNO.
+ *
+ * Indien deze software in opdracht werd uitgebracht, wordt voor de rechten en verplichtingen van opdrachtgever
+ * en opdrachtnemer verwezen naar de Algemene Voorwaarden voor opdrachten aan TNO, dan wel de betreffende
+ * terzake tussen de partijen gesloten overeenkomst.
+ */
+package org.example;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EntityJson implements Serializable {
+
+    private String stringProp;
+
+    private Long longProp;
+
+}
---
+++ src/main/java/org/example/create_table.sql
@@ -0,0 +1,5 @@
+create table entity_value
+(
+    id       integer primary key,
+    payload  jsonb not null
+);
